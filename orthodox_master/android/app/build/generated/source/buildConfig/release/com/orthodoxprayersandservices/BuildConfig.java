bb779ba4855680aa2e3c2d2e35da7190cd5bd2f2
zahidalidev <engrzahid612@gmail.com>
2020-08-18 20:25:56 +0500

    modules

---
+++ orthodox_master/android/app/build/generated/source/buildConfig/release/com/orthodoxprayersandservices/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.orthodoxprayersandservices;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.orthodoxprayersandservices";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 3145741;
+  public static final String VERSION_NAME = "1.31";
+}
---
+++ orthodox_master/node_modules/react-native-material-menu/src/MenuItem.js
@@ -0,0 +1,89 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  View,
+} from 'react-native';
+
+const Touchable = Platform.select({
+  android: TouchableNativeFeedback,
+  default: TouchableHighlight,
+});
+
+function MenuItem({
+  children,
+  disabled,
+  disabledTextColor,
+  ellipsizeMode,
+  onPress,
+  style,
+  textStyle,
+  ...props
+}) {
+  const touchableProps =
+    Platform.OS === 'android'
+      ? { background: TouchableNativeFeedback.SelectableBackground() }
+      : {};
+
+  return (
+    <Touchable
+      disabled={disabled}
+      onPress={onPress}
+      {...touchableProps}
+      {...props}
+    >
+      <View style={[styles.container, style]}>
+        <Text
+          ellipsizeMode={ellipsizeMode}
+          numberOfLines={1}
+          style={[
+            styles.title,
+            disabled && { color: disabledTextColor },
+            textStyle,
+          ]}
+        >
+          {children}
+        </Text>
+      </View>
+    </Touchable>
+  );
+}
+
+MenuItem.propTypes = {
+  children: PropTypes.node.isRequired,
+  disabled: PropTypes.bool,
+  disabledTextColor: PropTypes.string,
+  ellipsizeMode: PropTypes.string,
+  onPress: PropTypes.func,
+  style: TouchableHighlight.propTypes.style,
+  textStyle: Text.propTypes.style,
+  underlayColor: TouchableHighlight.propTypes.underlayColor,
+};
+
+MenuItem.defaultProps = {
+  disabled: false,
+  disabledTextColor: '#bdbdbd',
+  ellipsizeMode: Platform.OS === 'ios' ? 'clip' : 'tail',
+  underlayColor: '#e0e0e0',
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 48,
+    justifyContent: 'center',
+    maxWidth: 248,
+    minWidth: 124,
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: '400',
+    paddingHorizontal: 16,
+  },
+});
+
+export default MenuItem;
