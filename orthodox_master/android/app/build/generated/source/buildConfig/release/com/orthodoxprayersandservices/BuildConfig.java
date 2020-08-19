5632c25425c47eef04a999c8e731ac159848306d
zahidalidev <engrzahid612@gmail.com>
2020-08-19 16:28:54 +0500

    release 1.40

--- orthodox_master/android/app/build/generated/source/buildConfig/release/com/orthodoxprayersandservices/BuildConfig.java
+++ orthodox_master/android/app/build/generated/source/buildConfig/release/com/orthodoxprayersandservices/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.orthodoxprayersandservices";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 3145742;
-  public static final String VERSION_NAME = "1.32";
+  public static final int VERSION_CODE = 3145743;
+  public static final String VERSION_NAME = "1.40";
 }
